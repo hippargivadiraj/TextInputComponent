6d7b20ec7ca3092b3f60e1768194e5b062ebc1f8
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-06-07 13:44:50 -0700

    Initial Commit

---
+++ TextInputComponent/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TextInputComponent
+//
+//  Created by Vadiraj Hippargi on 6/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextInputComponent/TextInputComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextInputComponentApp.swift
+//  TextInputComponent
+//
+//  Created by Vadiraj Hippargi on 6/7/21.
+//
+
+import SwiftUI
+
+@main
+struct TextInputComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
